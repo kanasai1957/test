fa7b92954fd26b7e6408ad5052cfc42e095c2b5a
Samuel Lie <kanasai1957@gmail.com>
2018-10-11 21:13:57 +0800

    nothing liao

--- first.c
+++ first.c
@@ -4,6 +4,10 @@ int main(){
     pasdsadasdasdasdsd
 }
 void somefunction(){
+<<<<<<< HEAD
 printf("Changed liao at 8.56 p.m.");}
 printf("hello world")
     pasdsadasdasdasdsd
+=======
+printf("Changed liao at 8.56 p.m.");}
+>>>>>>> parent of 505216f... simply changed
