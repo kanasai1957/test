589135ef3108e5ce72d95f79f3f2f4e5e32ada39
Samuel Lie <kanasai1957@gmail.com>
2018-10-11 21:00:54 +0800

    test build

--- first.c
+++ first.c
@@ -1,11 +1,5 @@
 #include <stdiadadasdsdo.h>
 int main(){
     printf("hello world")
-    printf("hello world")
-    printf("hello world")
-    printf("hello world")
-    printf("hello world")
     pasdsadasdasdasdsd
 }
-void somefunction(){
-printf("Changed liao at 8.56 p.m.");}
