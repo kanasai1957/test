f8d33c4f38992e4d35a4c2b863b836f8ff85ffbd
Samuel Lie <kanasai1957@gmail.com>
2018-10-11 21:04:55 +0800

    sohai

--- first.c
+++ first.c
@@ -1,5 +1,24 @@
 #include <stdiadadasdsdo.h>
 int main(){
     printf("hello world")
+    printf("hello world")
+
+    printf("hello world")
     pasdsadasdasdasdsd
 }
+printf("hello world")
+
+printf("hello world")
+v
+printf("hello world")
+
+printf("hello world")
+
+printf("hello world")
+v
+printf("hello world")
+printf("hello world")
+
+printf("hello world")
+v
+printf("hello world")
