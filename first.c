610687edb2c2b9d553744585780b135887cba1ea
Samuel Lie <kanasai1957@gmail.com>
2018-10-11 20:42:07 +0800

    some changes

--- first.c
+++ first.c
@@ -1,4 +1,15 @@
 #include <stdio.h>
 int main(){
     printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    printf("hello world")
+    
+
+
+    printf("hello world")
 }
