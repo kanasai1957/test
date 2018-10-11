8777e84a8a962b5a62a4f431f41ec52eda6a674c
Samuel Lie <kanasai1957@gmail.com>
2018-10-11 21:13:12 +0800

    mistake

--- first.c
+++ first.c
@@ -3,10 +3,7 @@ int main(){
     printf("hello world")
     pasdsadasdasdasdsd
 }
+void somefunction(){
+printf("Changed liao at 8.56 p.m.");}
 printf("hello world")
     pasdsadasdasdasdsd
-    printf("hello world")
-    pasdsadasdasdasdsd
-    printf("hello world")
-    pasdsadasdasdasdsd
-    
